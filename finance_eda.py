bf79be8802569f37d68584ea8bbf3e009c8a90e2
AbhisheakSaraswat <60028984+AbhisheakSaraswat@users.noreply.github.com>
2024-05-04 06:10:46 +0000

    modified:   finance_eda.py

--- finance_eda.py
+++ finance_eda.py
@@ -3,5 +3,6 @@ import pandas as pd
 file_path = r"C:\Users\AEPAC\Desktop\KnowledgeHut\2024\Data Analyst - 20th Jan\Tableau\Tableau 1.1\Superstore.xls"
 print("How you are doing today?")
 file_path = "here your file path"
+print("I have added something")
 df = pd.read_excel(file_path)
 print(df)

7ac215133436b5568f35d6667085f42a09d4b8a6
Rohit-Baisoya <168855230+Rohit-Baisoya@users.noreply.github.com>
2024-05-04 19:41:33 +0530

    Update finance_eda.py
    To create a new branch

--- finance_eda.py
+++ finance_eda.py
@@ -1,5 +1,6 @@
 import pandas as pd
 # readin the file from excel
+# i have created new branch as Rohit_Branch
 file_path = r"C:\Users\AEPAC\Desktop\KnowledgeHut\2024\Data Analyst - 20th Jan\Tableau\Tableau 1.1\Superstore.xls"
 print("How you are doing today?")
 file_path = "here your file path"
